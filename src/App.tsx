d315ed8e87456f1c1a964dd860de4803a4612c7d
yibinllin <yibinllin@tencent.com>
2023-11-19 21:11:54 +0800

    feat: heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,14 @@
-import { PageLayout, Box, Textarea, Button } from "@primer/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ThemeProvider,
+  BaseStyles,
+  PageLayout,
+  Textarea,
+  Button,
+  Heading,
+  ThemeProviderProps,
+} from "@primer/react";
 import { useState } from "react";
 
 enum Transformer {
@@ -17,6 +27,18 @@ const TransformerMap = {
   },
 };
 
+enum ColorMode {
+  AUTO = 0,
+  DARK,
+  LIGHT,
+}
+
+const ColorModeMap: Record<number, ThemeProviderProps["colorMode"]> = {
+  [ColorMode.AUTO]: "auto",
+  [ColorMode.LIGHT]: "light",
+  [ColorMode.DARK]: "dark",
+};
+
 const doTransform = (
   curr: Transformer,
   key: "encoder" | "decoder",
@@ -33,57 +55,93 @@ const doTransform = (
 function App() {
   const [rawText, setRawText] = useState<string>("");
   const [encodedText, setEncodedText] = useState<string>("");
+  const [colorMode, setColorMode] = useState<ColorMode>(0);
 
   const [currTrans, setCurrTrans] = useState<Transformer>(
     Transformer.URIComponent
   );
 
+  // const clear = () => {
+  //   setRawText("");
+  //   setEncodedText("");
+  // };
+
   return (
-    <PageLayout
-      sx={{ bg: "canvas.default", minHeight: "100vh" }}
-      padding="none"
-    >
-      <PageLayout.Content padding="normal" sx={{ height: "100vh" }}>
-        <Textarea
-          block
-          resize="none"
-          value={rawText}
-          onChange={(event) => {
-            const newRawText = event?.target?.value || "";
-            const newEncodedText =
-              doTransform(currTrans, "encoder", newRawText) || "";
-            setRawText(newRawText);
-            setEncodedText(newEncodedText);
-          }}
-        />
-        <Textarea
-          block
-          resize="none"
-          value={encodedText}
-          onChange={(event) => {
-            const newEncodedText = event?.target?.value || "";
-            const newRawText =
-              doTransform(currTrans, "decoder", newEncodedText) || "";
-            setRawText(newRawText);
-            setEncodedText(newEncodedText);
-          }}
-        />
-      </PageLayout.Content>
-      <PageLayout.Pane sx={{ bg: "canvas.subtle" }}>
-        <Button
-          onClick={() => {
-            if (currTrans === Transformer.URI) {
-              setCurrTrans(Transformer.URIComponent);
-            } else {
-              setCurrTrans(Transformer.URI);
-            }
-          }}
-        >
-          {Transformer[currTrans]}
-        </Button>
-      </PageLayout.Pane>
-    </PageLayout>
+    <React.StrictMode>
+      <ThemeProvider colorMode={ColorModeMap[colorMode]}>
+        <BaseStyles>
+          <PageLayout
+            sx={{ bg: "canvas.default", minHeight: "100vh" }}
+            containerWidth="full"
+            padding="none"
+            columnGap="none"
+          >
+            <PageLayout.Content padding="normal" sx={{ height: "100vh" }}>
+              <Heading sx={{ fontSize: 3 }}>Decoded</Heading>
+              <Textarea
+                block
+                resize="none"
+                value={rawText}
+                onChange={(event) => {
+                  const newRawText = event?.target?.value || "";
+                  const newEncodedText =
+                    doTransform(currTrans, "encoder", newRawText) || "";
+                  setRawText(newRawText);
+                  setEncodedText(newEncodedText);
+                }}
+                sx={{
+                  height: "30vh",
+                  marginBottom: 20,
+                }}
+              />
+              <Heading sx={{ fontSize: 3 }}>Encoded</Heading>
+              <Textarea
+                block
+                resize="none"
+                value={encodedText}
+                onChange={(event) => {
+                  const newEncodedText = event?.target?.value || "";
+                  const newRawText =
+                    doTransform(currTrans, "decoder", newEncodedText) || "";
+                  setRawText(newRawText);
+                  setEncodedText(newEncodedText);
+                }}
+                sx={{
+                  height: "30vh",
+                }}
+              />
+            </PageLayout.Content>
+            <PageLayout.Pane
+              position="start"
+              padding="normal"
+              sx={{ bg: "canvas.subtle" }}
+            >
+              <Button
+                block
+                onClick={() => {
+                  if (currTrans === Transformer.URI) {
+                    setCurrTrans(Transformer.URIComponent);
+                  } else {
+                    setCurrTrans(Transformer.URI);
+                  }
+                }}
+              >
+                {Transformer[currTrans]}
+              </Button>
+              <Button
+                block
+                onClick={() => {
+                  setColorMode((colorMode + 1) % 3);
+                }}
+              >
+                {ColorModeMap[colorMode]}
+              </Button>
+            </PageLayout.Pane>
+          </PageLayout>
+        </BaseStyles>
+      </ThemeProvider>
+    </React.StrictMode>
   );
 }
 
-export default App;
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);

de0015184ef5802f4c8e53100a5c1dff92b526a0
yibinllin <yibinllin@tencent.com>
2023-11-19 20:40:47 +0800

    feat: primer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,89 @@
-function App() {
+import { PageLayout, Box, Textarea, Button } from "@primer/react";
+import { useState } from "react";
 
-  return (
-    <>
-      hello
-    </>
-  )
+enum Transformer {
+  URIComponent = 0,
+  URI,
 }
 
-export default App
+const TransformerMap = {
+  [Transformer.URIComponent]: {
+    encoder: encodeURIComponent,
+    decoder: decodeURIComponent,
+  },
+  [Transformer.URI]: {
+    encoder: encodeURI,
+    decoder: decodeURI,
+  },
+};
+
+const doTransform = (
+  curr: Transformer,
+  key: "encoder" | "decoder",
+  text: string
+): string => {
+  try {
+    return TransformerMap[curr][key](text);
+  } catch (e) {
+    console.log(e);
+    return "";
+  }
+};
+
+function App() {
+  const [rawText, setRawText] = useState<string>("");
+  const [encodedText, setEncodedText] = useState<string>("");
+
+  const [currTrans, setCurrTrans] = useState<Transformer>(
+    Transformer.URIComponent
+  );
+
+  return (
+    <PageLayout
+      sx={{ bg: "canvas.default", minHeight: "100vh" }}
+      padding="none"
+    >
+      <PageLayout.Content padding="normal" sx={{ height: "100vh" }}>
+        <Textarea
+          block
+          resize="none"
+          value={rawText}
+          onChange={(event) => {
+            const newRawText = event?.target?.value || "";
+            const newEncodedText =
+              doTransform(currTrans, "encoder", newRawText) || "";
+            setRawText(newRawText);
+            setEncodedText(newEncodedText);
+          }}
+        />
+        <Textarea
+          block
+          resize="none"
+          value={encodedText}
+          onChange={(event) => {
+            const newEncodedText = event?.target?.value || "";
+            const newRawText =
+              doTransform(currTrans, "decoder", newEncodedText) || "";
+            setRawText(newRawText);
+            setEncodedText(newEncodedText);
+          }}
+        />
+      </PageLayout.Content>
+      <PageLayout.Pane sx={{ bg: "canvas.subtle" }}>
+        <Button
+          onClick={() => {
+            if (currTrans === Transformer.URI) {
+              setCurrTrans(Transformer.URIComponent);
+            } else {
+              setCurrTrans(Transformer.URI);
+            }
+          }}
+        >
+          {Transformer[currTrans]}
+        </Button>
+      </PageLayout.Pane>
+    </PageLayout>
+  );
+}
+
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider, BaseStyles } from "@primer/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider colorMode="auto">
+      <BaseStyles>
+        <App />
+      </BaseStyles>
+    </ThemeProvider>
+  </React.StrictMode>
+);
